e73dfbc7c91e5f73c62840db44c4ede59fc41780
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-29 21:26:26 +0300

    Added lists.h - header file

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,13 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
- *struct listint_s single linked list
- *@n: integer
- *@next: points to the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -18,7 +20,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
 listint_t *insert_node(listint_t **head, int number);
 
-#endif /* LISTS_H *
+#endif /* LISTS_H */
