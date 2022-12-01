c0784bc3d31c040f57983dd67bb600215272e698
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-01 21:38:31 +0300

    Added 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     a = 10
     b = 5
     print("{0} + {1} = {2}".format(a, b, add(a, b)))
-    print("{0} + {1} = {2}".format(a, b, sub(a, b)))
-    print("{0} + {1} = {2}".format(a, b, mul(a, b)))
-    print("{0} + {1} = {2}".format(a, b, div(a, b)))
+    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    print("{0} / {1} = {2}".format(a, b, div(a, b)))
