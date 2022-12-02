0a9ae81716ba32eda7e0cbe27e9a9b973fcbdbb2
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-02 18:44:40 +0300

    Added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arguments = []
+    if len(sys.argv) == 2:
+        print("1 argument")
+    else:
+        print("{} arguments".format(len(sys.argv)-1))
+        for arg in sys.argv:
+            arguments.append(arg)
+
+        for i in range(1, len(arguments)):
+            print("{0}:{1}".format(i, arguments[i]))
