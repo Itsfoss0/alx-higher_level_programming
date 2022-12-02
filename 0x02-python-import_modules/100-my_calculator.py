c6c523bacc77b33daea32b1b7bb27c6838c7690d
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-02 22:41:08 +0300

    Added 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,4 +6,21 @@ if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1);
-        
+
+    else:
+        opcode = str(sys.argv[2])
+        operand_1 = int(sys.argv[1])
+        operand_2 = int(sys.argv[3])
+
+        if  opcode == '+':
+            print("{0} + {1} = {2}".format(operand_1, operand_2, add(operand_1, operand_2)))
+        elif opcode == '-':
+            print("{0} - {1} = {2}".format(operand_1, operand_2, sub(operand_1, operand_2)))
+        elif opcode == '*':
+            print("{0} * {1} = {2}".format(operand_1, operand_2, mul(operand_1, operand_2)))
+        elif opcode == '/':
+            print("{0} / {1} = {2}".format(operand_1, operand_2, div(operand_1, operand_2)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+            
