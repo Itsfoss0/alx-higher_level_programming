2e50c683004f07e5f015b0edd1756fd8544ef4c8
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-02 19:05:59 +0300

    Added 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     arguments = []
     if len(sys.argv) == 2:
-        print("1 argument")
+        print("1 argument:")
         print("{0}: {1}".format((len(sys.argv)-1), sys.argv[1]))
     else:
         for arg in sys.argv:
@@ -13,7 +13,7 @@ if __name__ == "__main__":
             print("{0} arguments.".format(len(sys.argv)-1))
 
         else:
-            print("{} arguments".format(len(sys.argv)-1))
+            print("{} arguments: ".format(len(sys.argv)-1))
 
             for i in range(1, len(arguments)):
                 print("{0}: {1}".format(i, arguments[i]))
