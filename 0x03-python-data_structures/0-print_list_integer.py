16d00b0ef5d4f58c27773ca755066bcd216c55ec
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-03 20:04:49 +0300

    Added 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Prints all integers of a list"""
     for pos in range(0, len(my_list)):
-        print('{0:d}'.format(my_list[pos]))
+        print("{0:d}".format(my_list[pos]))
