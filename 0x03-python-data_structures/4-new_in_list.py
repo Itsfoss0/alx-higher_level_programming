0320ffbed54b2f85cb78a611ed3469cdecb22414
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-04 08:23:44 +0300

    Added 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Return new list with item at position
+        idx set to elelement
+    """
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    else:
+        new_list = my_list
+        new_list[idx] = element
+        return new_list
