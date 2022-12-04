10986be7f3361e68edfca4c2a7bf56b28c908ae0
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-04 18:15:15 +0300

    Added lists.h header files

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +12,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
