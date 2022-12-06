fa795b7a93257f355fb42445dfda5682546d6e4b
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-06 16:48:13 +0300

    Added 0-remove_float.py

--- 0x03-python-data_structures/practice/0-remove_float.py
+++ 0x03-python-data_structures/practice/0-remove_float.py
@@ -1,4 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Given a list of numbers, remove floats (numbers with decimals)
 """
+original_list = [2,3.75,.04,59.354,6,7.7777,8,9]
+only_ints = [ x for x in original_list if isinstance(x, int)]
+print(only_ints)
