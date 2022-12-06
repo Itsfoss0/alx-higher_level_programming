f5ccbe2e4b2f80ab025fcafc33f42117aa45a659
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-06 21:45:28 +0300

    Refactored code to use functions

--- 0x03-python-data_structures/practice/8-less_4.py
+++ 0x03-python-data_structures/practice/8-less_4.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """Find all of the words in a string that are less than 4 letters"""
 
-string = "I love programming in python and C"
-less_4 = [x for x in string.split(" ") if len(x) < 4]
-print(less_4)
+def less_than_4(string):
+    return ([x for x in string.split(" ") if len(x) < 4])
+
+
+my_string = "I love programming in python and C"
+string_2 = "Did John join the DevOps team?"
+print(less_than_4(my_string))
+print(less_than_4(string_2))
