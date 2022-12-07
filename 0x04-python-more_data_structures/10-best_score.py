b8125d853a6e5ede5e246c605084a12832eb29fc
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-07 21:06:46 +0300

    Added 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """find the max of a dictionary"""
     if not a_dictionary:
         return None
-    return max(a_dictionary, key=a_dictionary.get)
-print(best_score({"John": 33, "Doe": 23}))
+    return max(a_dictionary, key=a_dictionary.get())
