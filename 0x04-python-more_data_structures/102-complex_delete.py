16861f5c813ae856a86f1376372d1cff550c5f10
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-09 10:07:58 +0300

    Added 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     if value not in dictionary.items():
         return a_dictionary
-    for val in a_dictionary.values():
+    for key, val in a_dictionary.items():
         if val == value:
-            del a_dictionary[value]
+            del a_dictionary[val]
     return a_dictionary
