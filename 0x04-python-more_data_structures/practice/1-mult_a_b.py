ab453d89d8f00bded2e2ea0beaeab4d3d94d3ecd
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-09 18:40:23 +0300

    Added 1-mult_a_b.py

--- 0x04-python-more_data_structures/practice/1-mult_a_b.py
+++ 0x04-python-more_data_structures/practice/1-mult_a_b.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""Write a function which takes two arguments: a and b 
+    and returns the multiplication of them: a*b """
 mult = lambda a, b :  a * b
 print(mult(2,3)) # should print 6 
