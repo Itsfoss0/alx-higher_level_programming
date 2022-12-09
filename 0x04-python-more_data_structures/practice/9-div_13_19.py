308dc1092d7d9b6e7f5ccbb5733795e443465952
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-09 21:21:09 +0300

    Added 9-div_13_19.py

---
+++ 0x04-python-more_data_structures/practice/9-div_13_19.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""find numbers divisible by nineteen or thirteen from a list of numbers"""
+divibles = lambda int_list : [x for x in int_list if (x % 13 == 0 ) and (x % 19 == 0 )]
