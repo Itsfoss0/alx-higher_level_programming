d2ebfb94906bdc4711caff199226e5f20917ca43
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-09 20:54:56 +0300

    Added 7-sorted.py

--- 0x04-python-more_data_structures/practice/7-sorted.py
+++ 0x04-python-more_data_structures/practice/7-sorted.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 """rearrange positive and negative numbers in a given array"""
-rearranged = lambda array : sorted(array)
+rearranged = lambda array : sorted(array, reverse=True)
+
+print(rearranged([-1, -3, -7, 4, 5, 9, 2]))
