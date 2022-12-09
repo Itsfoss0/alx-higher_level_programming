6e4ca5751da56ca4923ade96b2929e8988a49815
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-09 21:15:29 +0300

    Added 8-str_len.py

--- 0x04-python-more_data_structures/practice/8-str_len.py
+++ 0x04-python-more_data_structures/practice/8-str_len.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+"""find the values of length six in a given list using"""
+less_6 = lambda strings : [x for x in strings if len(x) == 6]
+print(less_6(["John", "Doe", "Martin", "Riggs"]))
