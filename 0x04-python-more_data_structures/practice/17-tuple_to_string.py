5f83226344ba3d17fa51aaa93144aa7a94a8194e
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-12 08:59:05 +0300

    Added 17-tuple_to_string.py

--- 0x04-python-more_data_structures/practice/17-tuple_to_string.py
+++ 0x04-python-more_data_structures/practice/17-tuple_to_string.py
@@ -2,4 +2,13 @@
 """convert a given list of tuples to a list of strings using map function"""
 
 colors = [('red', 'pink'), ('white', 'black'), ('orange', 'green')]
-print(list(map(lambda x : "".join(x), colors)))
+print(list(map(lambda x : " ".join(x), colors)))
+
+def add_tuple_elements(tup_list, dest):
+    for i in range(len(tup_list)):
+        for element in tup_list[i]:
+            dest.append(element)
+
+    return dest
+
+print(add_tuple_elements(colors, dest=[]))
