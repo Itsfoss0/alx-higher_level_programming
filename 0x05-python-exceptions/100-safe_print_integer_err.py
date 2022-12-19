09d737b7b5cb4331371a6678a8c2cd870a3e09c2
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-19 20:13:56 +0300

    Added 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, NameError, TypeError) as e:
-        sys.stderr.write("Exception {}".format(e))
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
