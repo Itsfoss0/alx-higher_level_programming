5df26f81fc2fa294e1048a501d6d6eafbdcc7179
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-19 13:05:59 +0300

    Added 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x+1):
+            print(my_list[i], end="")
+    except IndexError:
+        pass
+        
+        
