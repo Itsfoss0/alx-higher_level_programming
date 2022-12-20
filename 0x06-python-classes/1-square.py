30b51b83145559b8ceeef83f4ad91df65a183735
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-20 17:53:35 +0300

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Working with OOP in python"""
+
+
+class Square():
+    """Implementing the square class"""
+    def __init__(self, size):
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
