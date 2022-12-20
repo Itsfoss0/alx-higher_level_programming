26b1ced4c1cbca025321b89fc9b2d49ae288e2af
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-20 18:11:45 +0300

    Added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Working with OOP in python"""
+
+
+class Square():
+    """Implementing the square class"""
+    def __init__(self, size=0):
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0 ")
+        else:
+            self.__size = size
