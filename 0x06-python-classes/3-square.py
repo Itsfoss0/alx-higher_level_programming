790227628df4d0b0f5cf35445467591ccb3b59aa
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-21 19:13:41 +0300

    Added 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Working with OOP in python"""
+"""class Square that defines a square"""
 
 
-class Square():
-    """Implementing the square class"""
+class Square:
+    """square class with it's size and proper validation"""
+
     def __init__(self, size=0):
-        """ initialize the class
-        Args:
-           size (int): size of the square
-        """
-        if not isinstance(size, int):
+        if (not isinstance(size, int)):
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0 ")
-        else:
-            self.__size = size
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Compute the area of the current square
-        Args:
-            None """
-        return (self.__size * 4)
+        return self.__size ** 2
