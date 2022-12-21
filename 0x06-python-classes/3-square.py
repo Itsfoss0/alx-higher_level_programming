5ddfc5158d0d82614d0ab51cd9c6456757c66187
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-21 19:15:16 +0300

    Added 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,8 +3,7 @@
 
 
 class Square:
-    """square class with it's size and proper validation"""
-
+    """Implementing square class """
     def __init__(self, size=0):
         if (not isinstance(size, int)):
             raise TypeError("size must be an integer")
