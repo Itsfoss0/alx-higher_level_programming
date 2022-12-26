5f48f01976a0046586d30489a17a031fc0c6b90f
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-26 16:21:35 +0300

    Added 0-class.py

--- 0x06-python-classes/practice/0-class.py
+++ 0x06-python-classes/practice/0-class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 class Vehicle:
+    """Vehicle class to represent a vehicle"""
 
     def __init__(self, max_speed, milleage):
+        """
+        Initialize the values 
+        Args:
+            max_speed (int): maximum speed of the vehicle
+            milleage (int): 
+        """
         self.max_speed = max_speed
         self.milleage = milleage
