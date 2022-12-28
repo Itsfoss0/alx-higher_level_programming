b6330b08fa0755ce6104a960525fa4e2013ba6fe
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-28 18:21:19 +0300

    Implemented Changing branches

--- 0x06-python-classes/practice/5-credit_card.py
+++ 0x06-python-classes/practice/5-credit_card.py
@@ -6,7 +6,7 @@ class CreditCard:
 
         Args:
             name (str)      -> Name of the client
-            account(str)    -> Account Number of the client
+            account(str)    -> Account Number of the client, this cant be changed once assinged
             bank(str)       -> The Bank Branch of the clinet
             limit(int)      -> The spending limit on the credit card
             balance(int)    -> The balance on the card, by default a CC has a 0 balance
@@ -21,5 +21,39 @@ class CreditCard:
     def name(self):
         """Get the name of the client"""
         return self.__name
+    
+    @name.setter
+    def name(self, new_name):
+        """Mutate the name of the client, for some reason
+        
+        Args:
+            new_name (str) -> the new name
+        """
+        self.name = new_name
+
+    @property
+    def account(self):
+        """Get the account number"""
+        return self.__account
+
+    @property
+    def bank(self):
+        """Get the branch of the client"""
+        return self.__bank
+    
+    @bank.setter
+    def bank(self, bank):
+        """Change branch to another branch
+        
+        Args:
+            bank (str) -> the new branch
+        """
+        if self.__bank != bank:
+            self.__bank = ban
+            return ("Switched branch from to {}".format(bank))
+        else:
+            return "Cannot Switch to the same branch"
+
+
     
 
