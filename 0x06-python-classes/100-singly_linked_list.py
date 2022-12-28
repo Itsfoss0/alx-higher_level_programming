a003e3aaca8895a1b4b0a744dd4b0476da434bd3
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-28 17:12:48 +0300

    Adde 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -24,13 +24,34 @@ class Node:
     def next_node(self, value):
         if (not isinstance(value, Node) or value is not None):
             raise TypeError("next_node must be a Node Object")
+        else:
+            self.__next_node = value
 
 class SinglyLinkedList:
     """A list of Nodes that are linked singly"""
     def __init__(self):
+        self.__head = None
 
+    def sorted_insert(self, value):
+        """Insert a new node into the SLL"""
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while (temp.next_node is not None and temp.next_node.data < value):
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
 
-    def __repr__(self):
-        return "{}"
-
-        
+        def __str__(self):
+            """Define how print(SinglyLinkedLits()) would be"""
+            node_values = []
+            tmp = self.__head
+            while tmp is not None:
+                tmp = tmp.next_node
+            return ("\n".join(node_values)
