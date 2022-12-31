488302c90077f77b9990ffbac80973ad903e3ea7
Ebenezer <akhonyaebenezer@gmail.com>
2022-12-31 18:56:55 +0300

    Added 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,11 +6,14 @@ def add_integer(a, b=98):
         b(int) -> The second arg, if not passed, 98 is assumed to be the value
     Returns an int
     """
-    if not (isinstance(a, int)) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    elif not (isinstance(b, int)) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    else:
-        if isinstance(a, float) or isinstance(b, float):
-            return (int(a) + int(b))
-        return a + b
+    try: 
+        if not (isinstance(a, int)) and not isinstance(a, float):
+            raise TypeError("a must be an integer")
+        elif not (isinstance(b, int)) and not isinstance(b, float):
+            raise TypeError("b must be an integer")
+        else:
+            if isinstance(a, float) or isinstance(b, float):
+                return (int(a) + int(b))
+            return a + b
+    except Exception as e:
+        return (e)
