1b6a1235dc6d20185cdb574cdd3ff2d2f36bb016
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-01 21:15:36 +0300

    Added 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import unittest
-
 max_integer = __import__('6-max-integer').max_integer
+
+
 class TestMaxInteger(unittest.TestCase):
     """Defining unit tests for max_integer function"""
     def test_maxinteger_sorted(self):
         """Test for a sorted list"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        
+
     def test_maxinteger_unsorted(self):
         """Tests for an unsorted list"""
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
