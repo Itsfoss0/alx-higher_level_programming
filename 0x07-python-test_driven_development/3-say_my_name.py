20fa4f565d0d2eecb3a91c0a81cfffe94a5c2698
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-01 17:03:09 +0300

    Added 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    """Function to return formated name
+    Args:
+        first_name (str) -> The first name
+        last_name (str)  -> The last name
+    Returns:
+        'My name is <first_name> <last_name>
+    """
     try:
         if not isinstance(first_name, str):
             raise TypeError("first_name must be a string")
