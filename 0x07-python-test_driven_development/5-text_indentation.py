ce9b4045d8d1f76c282c4671dc0d5a140e03f009
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-01 19:33:53 +0300

    Added 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 def text_indentation(text):
     try:
-        c = 0
-        while c < len(text) and text[c] == ' ':
-            c += 1
-        # Dont know what I did right here
-        while c < len(text):
-            print(text[c], end="")
-            if text[c] == "\n" or text[c] in ".?:":
-                if text[c] in ".?:":
-                    print("\n")
+        if not isinstance(text, str):
+            raise TypeError("text must be a string")
+        else:
+            c = 0
+            while c < len(text) and text[c] == ' ':
                 c += 1
-                while c < len(text) and text[c] == ' ':
+            # Dont know what I did right here
+            while c < len(text):
+                print(text[c], end="")
+                if text[c] == "\n" or text[c] in ".?:":
+                    if text[c] in ".?:":
+                        print("\n")
                     c += 1
-                continue
-            c += 1
+                    while c < len(text) and text[c] == ' ':
+                        c += 1
+                    continue
+                c += 1
     except Exception as exc:
         return (exc)
