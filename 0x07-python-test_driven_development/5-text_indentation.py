bc0c8502c72ac013c3b829e3ed43fd966ee25f29
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-01 18:54:18 +0300

    Added 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 def text_indentation(text):
     try:
-
+        if not isinstance(text, str):
+            raise TypeError("text must be a string")
+        else:
+            for character in text.strip():
+                if character not in [".", "?", ":"]:
+                    print(character, end="")
+                else:
+                    print()
+                    print()
     except Exception as exc:
         return exc
+
+print(text_indentation("I hate John Doe so much.I'd really love to kill him and his: "))
