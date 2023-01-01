c4af707047bf6bace8599e83c4f77f08b42a13b2
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-01 16:50:18 +0300

    Added 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def print_square(size):
+    """ Function to print squares of '#' 
+    Args:
+        size(int/float) -> number of "#"'s to print
+                        -> Must be an int or a float >= 0 
+    Returns nothing except if an exception occured, 
+    in which case the exception raised is returned
+    """"
     try:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
