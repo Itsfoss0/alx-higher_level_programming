67c8f8a19d851e2cf0b9f21dcd294f18a1667f90
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-01 16:45:10 +0300

    Added 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def print_square(size):
+    try:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif isinstance(size, float) and size < 0:
+            raise ValueError("size must be an integer")
+        else:
+            for row in range(size):
+                for column in range(size):
+                    print("#", end="")
+                print()
+    except Exception as e:
+        return (e)
+
+print_square(3)
+print(print_square(2.0))
+print(print_square(0))
+print(print_square(-1))
