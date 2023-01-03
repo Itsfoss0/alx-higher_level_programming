8b3ae7fd88cea610d2eb96ec2ad9c810bfeddbee
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-03 14:39:55 +0300

    Added 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defining my rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class with setters and getters"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Width getter
+        Args:
+            None
+        Returns:
+            self.__width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Height getter
+        Args:
+            None
+        Returns:
+            self.__width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter
+        Args:
+            value(int)
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
