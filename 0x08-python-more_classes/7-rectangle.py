20d133cb881b6ffe61b50f96f1910294ba416d1b
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-03 20:08:45 +0300

    Added 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,10 +6,9 @@ class Rectangle:
     """Rectangle class with setters and getters"""
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-
         """Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
@@ -17,7 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -65,14 +64,14 @@ class Rectangle:
 
     def __str__(self):
         """String representation of object
-        of the Rectangle class
+        of the Rectangle class using the 'print_symbol'
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
