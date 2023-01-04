ce1b29ef613164d8d8f0304efef4c845234e8e4a
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-04 18:32:13 +0300

    Added 100-magic_string.py

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_string(lis=[]):
+    """Returns 'Best School' n times'
+    Args:
+        None
+    Returns:
+        "Best School"
+    """
+    lis += ["BestSchool"]
+    return ", ".join(lis)
