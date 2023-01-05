49ceaacdde02e95457ab50225475840c291ba8d7
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-05 13:09:03 +0300

    Added 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,7 +10,7 @@ def say_my_name(first_name, last_name=""):
     try:
         if not isinstance(first_name, str):
             raise TypeError("first_name must be a string")
-        elif not isinstance(last_name, str):
+        if not isinstance(last_name, str):
             raise TypeError("last_name must be a string")
         else:
             return ("My name is {} {}".format(first_name, last_name))
