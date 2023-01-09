f9a1576966ca19878644e13ace3795f54d6ac018
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-09 23:25:55 +0300

    Added 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+""" Module to assert if an object is an instance 
+of a class that inherited from the specifed classs
+"""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of class that inherits from a_class.
+    Args:
+        obj (any) ->  The object to check.
+        a_class (type) ->  The class to match the type of obj to.
+    Returns:
+        True if class of obj inherits a_class; False Otherwise
+    """
+    obj_class = type(obj)
+    return issubclass(obj_class, a_class)
