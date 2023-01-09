302796aa116acd19f77bcfa6578c93b96b2e444a
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-09 22:30:07 +0300

    Added 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """Function to return all attributes and method of
+    an object
+    Args:
+        obj(object of any class) -> The object in question
+    Returns:
+        list
+    """
+    return [x for x in dir(obj)]
