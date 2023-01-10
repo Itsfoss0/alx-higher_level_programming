01203068aac33496eb669daceca0021af6761763
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-10 19:51:48 +0300

    Added 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""class to JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary representation <obj>'s class
+    Args:
+        obj(object) -> Any object
+    Returns:
+        obj.__dict__."""
+    return dir(obj)
