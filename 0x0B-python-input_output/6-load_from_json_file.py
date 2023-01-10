fe6a16ca474c1dc7fdae0c13a45c62b6142bc01e
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-10 18:32:24 +0300

    Added 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Deserialize an JSON from a file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from JSON
+    Args:
+        filename(str) -> The filename holding JSON
+    """
+    if filename:
+        with open(filename, 'r', encoding="utf-8") as f:
+            json.loads(f.read())
