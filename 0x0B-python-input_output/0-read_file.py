2b63fff23fcffc9d9c06545b8208a26e77389fac
Ebenezer <akhonyaebenezer@gmail.com>
2023-01-10 15:03:58 +0300

    Added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Read a file and print its content to stdout"""
+
+
+def read_file(filename=""):
+    """Read filename's content
+    Args:
+        filename(str) -> the name of the file
+    Returns:
+        None
+    """
+    if filename:
+        try:
+            with open(filename, "w", encoding="UTF-8") as file:
+                print(file.read())
+        except Exception as e:
+            return (e)
+    return
