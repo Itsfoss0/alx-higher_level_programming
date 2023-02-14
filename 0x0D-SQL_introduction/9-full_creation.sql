849d9307f220c2b8063878737bc7ad9391310270
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-15 02:27:35 +0300

    Added 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,6 @@
 -- Create another table 'second_table' 
-USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS `second_table`(
-    `id` INT,
-    `name` VARCHAR(256),
-    `score` INT
-);
-
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, 'John', 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, 'Alex', 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, 'Bob', 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4, 'George', 8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
