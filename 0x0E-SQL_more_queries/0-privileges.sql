03cafecfced2f1746c74b09236d005c919666c33
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-16 01:52:52 +0300

    Added 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS 'user_0d_1'@'localhost';
-SHOW GRANTS 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
