cc286e54821c9b311f157a416e0dc66943a32273
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-16 01:50:58 +0300

    Added 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Create a user user_0d_1 with the password user_0d_1_pwd
-CREATE USER  'user_0d_1'@'localhost' IDENTIFIED  BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXITS 'user_0d_1'@'localhost' IDENTIFIED  BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
