5ad1c7065547d4d816e6175b830d526be6cebad6
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-16 03:05:12 +0300

    Added 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 USE hbtn_0d_usa;
 
 SELECT * FROM `cities` 
-WHERE `id` = (SELECT `id` FROM `states` WHERE `name` = 'California')
+WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = 'California')
