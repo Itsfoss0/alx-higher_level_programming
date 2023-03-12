3cf48fedba94fd479e5916ed6fa0e3e69127c68e
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-12 11:18:02 +0300

    [bug]: Fix ordering

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
---lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
-SELECT score, name FROM second_table
-WHERE score >= 10 ORDER BY score DESC;
+--lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
