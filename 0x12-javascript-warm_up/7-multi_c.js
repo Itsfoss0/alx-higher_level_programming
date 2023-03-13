7f74be24beaa98a5235d5cac5d719927c6c2334f
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-13 16:16:07 +0300

    [lint] Fix semistandard linter

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let numberOfTimes = parseInt(process.argv[2]);
+if (!isNaN(numberOfTimes)) {
+  while (numberOfTimes) {
+    console.log('C is fun');
+    numberOfTimes -= 1;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
