1f83a374f7bc9282b5873d2b9b780df5b85c2ed9
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-13 21:36:51 +0300

    [lint]: Fix semistandard linter

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, theFunction){
-    number += 1;
-    return (theFunction(number));
+exports.addMeMaybe = function (number, theFunction) {
+  number += 1;
+  return (theFunction(number));
 };
