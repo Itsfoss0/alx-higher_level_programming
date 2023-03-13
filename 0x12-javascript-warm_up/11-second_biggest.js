8fe76d284d0918fed5f3df39a6b131c9a48b3b99
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-13 20:22:03 +0300

    [bug]: Fix sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
-
-function maxValue (iterable) {
-  let maxv = iterable[0];
-  for (let i = 0; i <= iterable.length; i++) {
-    if (iterable[i] > maxv) {
-      maxv = iterable[i];
-    }
-  }
-  return (maxv);
-}
-
-if (process.argv.length < 3) {
-  console.log('0');
-} else if (process.argv.length === 3) {
-  console.log('1');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  const argsArray = process.argv.slice(2, parseInt(process.argv.length));
-  const numsArray = argsArray.map(Number, argsArray);
-  console.log(maxValue(numsArray));
+  const numberArgs = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((x, y) => x - y);
+  console.log(numberArgs[numberArgs.length - 2]);
 }
