23d18c709546c1c78ab0644c25c5e263bfd49059
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-13 19:00:45 +0300

    [lint]: Fix semistandard linting

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
