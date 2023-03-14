3a3ae7359030990ce2d8cc62e32a605516935e41
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-14 16:50:01 +0300

    [bug]: Fix constructor

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,8 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (!(w < 1) || (h < 1)) {
-      this.width = w;
-      this.height = h;
+    if (!(w < 1) && !(h < 1)) {
+      [this.width, this.height] = [w, h];
     }
   }
 };
