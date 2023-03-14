baedc3cabfdeb9c126581f553615412fbbb4fb2a
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-14 19:47:30 +0300

    [bug]: Fix charPrint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 const ParentSquare = require('./5-square');
 
 module.exports = class Square extends ParentSquare {
-  constructor (size) {
-    super(size, size);
-  }
+//   constructor (size) {
+//     super(size, size);
+//   }
 
   charPrint (c) {
     if (c !== undefined) {
@@ -12,9 +12,7 @@ module.exports = class Square extends ParentSquare {
         console.log(String(c).repeat(this.size));
       }
     } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log('X'.repeat(this.size));
-      }
+      this.print();
     }
   }
 };
