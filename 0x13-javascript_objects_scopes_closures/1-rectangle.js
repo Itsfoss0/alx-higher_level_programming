c455c4030f20057dac4a4f4b34a41f4fc00c5a7a
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-14 16:43:25 +0300

    [chore]: Add 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (!(w < 1) || (h < 1)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 };
