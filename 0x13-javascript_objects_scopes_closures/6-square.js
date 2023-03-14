8e6147928e53ffb2ee8fa07fc772ff07386d73ec
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-14 19:44:32 +0300

    [bug]: Fixed charPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,4 +5,16 @@ module.exports = class Square extends ParentSquare {
   constructor (size) {
     super(size, size);
   }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.size; i++) {
+        console.log(String(c).repeat(this.size));
+      }
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        console.log('X'.repeat(this.size));
+      }
+    }
+  }
 };
