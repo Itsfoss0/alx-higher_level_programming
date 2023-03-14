4d85b387f32e0d5c2aaa6b279410c13fb10fb9cb
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-14 20:34:04 +0300

    [chore]: Add 8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+Ok, this is by far the worst algorithim I've written,
+with a time Complexity of O(n)
+
+Ill have to optimize this one :(:(:(
+*/
+exports.esrever = function (list) {
+  const listLen = list.length;
+  const newList = [];
+
+  for (let i = listLen; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return (newList);
+};
