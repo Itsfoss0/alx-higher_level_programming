885dbc49b903a90b52caca41e4a34d3f31f573ae
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-17 18:13:49 +0300

    [chore]: Add first task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+script to connect to MySQL server running on localhost
+on port 3306
+"""
+import sys
+import MySQLdb as db
+
+[USERNAME, PASSWORD, DB_NAME] = sys.argv[1:4]
+
+
+def connect_and_query() -> None:
+
+    """Connect to the database and execute query"""
+    try:
+        cnx = db.connect(user=USERNAME, passwd=PASSWORD, port=3306, db=DB_NAME)
+        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
+        cursor.execute('SELECT * FROM states ORDER BY `id` ASC;')
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+    except Exception as e:
+        return (e)
+
+
+if __name__ == "__main__":
+    connect_and_query()
