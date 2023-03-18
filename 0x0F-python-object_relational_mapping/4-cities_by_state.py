c525d4e5bab37b2834d01f54ec610fb6367b097b
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-18 22:41:44 +0300

    [chore]: Add fifth task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Lists all cities from the cities table of database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <username> \
+                            <password> \
+                             <database-name>
+"""
+import sys
+import MySQLdb as db
+
+
+def connect_and_query() -> None:
+
+    """Connect to the database and execute query"""
+    try:
+        cnx = db.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        cursor = cnx.cursor(cursorclass=db.cursors.Cursor)
+        cursor.execute('SELECT city.id, city.name, state.name\
+                        FROM cities as city\
+                        INNER JOIN states as state\
+                        ON city.state_id = state.id\
+                        ORDER BY city.id ASC;')
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+
+        cursor.close()
+        cnx.close()
+    except Exception as e:
+        return (e)
+
+
+if __name__ == "__main__":
+    connect_and_query()
