e344a3a1e5405fa5cd3c944659302a8ea2d6f3e0
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-19 23:46:54 +0300

    [chore]: Add state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Defines the state model, containing the State class and
+"""
 from sqlalchemy import create_engine, Integer, Column, String
 from sqlalchemy.orm import declarative_base
 
@@ -8,6 +10,11 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class
+        id -> state.id
+        name -> state.name
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True,
                 nullable=False, unique=True,
