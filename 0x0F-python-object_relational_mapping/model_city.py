27d7a32c327f4d8f0930ee72903ea0932166283b
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-20 02:33:22 +0300

    [chore]: Add city model

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+"""
+City Model definition. Defines the city class
+Inheriting from the Base model which is defined 
+in the model_state model
+"""
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City:
+    """
+    id (int):  city id
+    name (str): City name
+    state_id(int): ID of the state to which this city belongs
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
