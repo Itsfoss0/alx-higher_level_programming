a4f71790bb5ee63aa012e0701e7446fe2ad046ce
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-20 03:20:24 +0300

    [bug]: Fix model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """
-Defines the city model, containing the State class and
-should I just add here a bunch of random strings, texts
-and words to make this look longer and fool the checker?
+Defines a state model that contain the class definition
+ of a City and an instance Base = declarative_base()
 """
-from sqlalchemy import Integer, Column, String, ForeignKey
-from sqlalchemy.orm import declarative_base
-from model_state import Base
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
+from sqlalchemy import Column, ForeignKey, Integer, String, null
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """
-    State class
-        id -> citie.id
-        name -> state.name
-        state_id -> foreign key
-    """
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True,
-                nullable=False, unique=True,
-                autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+    inherits from Base (imported from model_state)
+    links to the MySQL table cities
 
-    def __repr__(self):
-        return "{}: {}".format(self.id, self.name)
+    class attribute id that represents a column of
+    an auto-generated, unique integer, can't be null and is a primary key
+
+    class attribute name that represents a column
+    of a string of 128 characters and can't be null
+
+    class attribute state_id that represents a column
+    of an integer, can't be null and is a foreign key to states.id
+
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
