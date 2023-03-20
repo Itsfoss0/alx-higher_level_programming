89cb37416792e2ea8e5f430792ca8805e23450b6
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-20 03:19:13 +0300

    [chore]: Add fourteenth task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
 """
-Connect to a database and perform some queries
-using an ORM - SQLAlchemy
+prints all City objects from the database hbtn_0e_14_usa
 """
+
 import sys
+from venv import create
+from sqlalchemy import create_engine, true
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from model_state import State
 from model_city import City
-from model_state import Base, State
-
-
-def connect_and_query(user: str, passwd: str, dbase: str) -> None:
-    """
-    Function to connect to a database and perform queries
-    Args:
-        user (str): mysql user
-        passwd (str): mysql password for `user`
-        dbase (str): database to connect to
-    """
-    try:
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-                               user, passwd, dbase, pool_pre_ping=True))
-        session_maker = sessionmaker(bind=engine)
-        session = session_maker()
-
-        for city, state in session.query(City, State)\
-            .filter(City.state_id == State.id)\
-                .order_by(City.id):
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
-    except Exception as e:
-        return e
-
 
 if __name__ == "__main__":
-    connect_and_query(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    for city, state in session.query(City, State)\
+        .filter(City.state_id == State.id)\
+            .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
