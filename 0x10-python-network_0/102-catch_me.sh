6dcf98da349558e49874b1c09a8deb09d71a03bf
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-31 01:49:28 +0300

    [Chore]: Add task ten

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Make request to 0.0.0.0:5000/catch_me
-curl -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me_3
+curl -L  -X PUT  -H 'Origin: School' localhost:5000/catch_me_3
