a16937b88b685c97f6760df1ed293e833bf5a59d
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-30 16:43:33 +0300

    [Chore]: Add task one

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #send a GET request to the url specified and print the size of response body to stdout
-curl -sI $1" | head -n 5 | tail -n 1 | cut -d ":" -f 2
+curl -sI "$1" | head -n 5 | tail -n 1 | cut -d ":" -f 2
