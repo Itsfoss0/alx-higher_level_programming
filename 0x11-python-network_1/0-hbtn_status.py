2af62f570620bbbe922712a5d9db5e9e556becb3
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-31 18:40:29 +0300

    [Chore]: Add task one

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-from urllib import request
+"""get https://alx-intranet.hbtn.io/status"""
+from urllib import request, error
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('utf-8')))
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        with request.urlopen(url) as response:
+            response = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(response)))
+            print("\t- content: {}".format(response))
+            print("\t- utf8 content: {}".format(response.decode('utf-8')))
+    except error.URLError:
+        print("Cannot connect to https://alx-intranet.hbtn.io/status")
