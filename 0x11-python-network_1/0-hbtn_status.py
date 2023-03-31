2a4e7395fec736f286e10ce267ef42e4513edd10
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-31 18:36:04 +0300

    [Chore]: Add task one

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-
-from urllib import request, error
-"""
-Fetch https://alx-intranet.hbtn.io/status
-and print the formated content to stdout
-"""
-
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    try:
-        with request.urlopen(url) as response:
-            response = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(response)))
-            print("\t- content: {}".format(response))
-            print("\t- utf-content: {}".format(response.decode('UTF-8')))
-    except error.URLError:
-        print("Check your internet connectivity and try again")
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
