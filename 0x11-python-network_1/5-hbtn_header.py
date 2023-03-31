63730da32e565dc17347d77ba7c168290d074b2a
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-31 22:09:25 +0300

    [Chore]: Add task five

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -16,7 +16,11 @@ def get_alx_intranet(url):
     Send a GET request to the url
     and print the X-Request-Id header value
     """
-    return get(url).headers['X-Request-Id']
+    if url:
+        try:
+            return get(url).headers.get('X-Request-Id')
+        except Exception as e:
+            return e
 
 
 if __name__ == "__main__":
