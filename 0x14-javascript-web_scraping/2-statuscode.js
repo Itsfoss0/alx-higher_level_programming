c78a51a7e9bb8406ad5e2306f84c6b18961b1a56
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-25 22:39:08 +0300

    [Chore]: Add task three

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
