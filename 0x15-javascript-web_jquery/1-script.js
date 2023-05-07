38e2dae6c4e63ebf36f5c8c9a78a94fac6636d27
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-07 18:28:21 +0300

    [Chore]: Add task two

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,24 @@
+const header = $('header');
+header.css('color', 'red');
+/*
+const addButton = $('.add-item');
+ let taskNo = 1;
+ addButton.on('click', function (){
+    const itemsList = $('.items');
+    if (taskNo >= 0){
+    $('.items').append(`<li> Task ${taskNo}</li>`);
+    taskNo += 1;
+    } else {
+        $('.items').append(`<li> Task 0</li>`);
+        taskNo += 1;
+    }
+    console.log(itemsList.html())
+ });
+
+ const deleteButton = $('.delete-item')
+
+ deleteButton.on('click', function () {
+    $('.items').find('li').last().remove()
+    taskNo -= 1
+ })
+ */
