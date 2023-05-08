4ba293be6068e717258de872d18ea26348c91779
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-08 15:45:37 +0300

    [Chore]: Add advanced task - 2

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#add_item').on('click', () => {
+        $('.my_list').append('<li>Item</li>');
+    });
+
+    $('#remove_item').on('click', () => {
+        $('.my_list').find('li').last().remove()
+    });
+
+    $('#clear_list').on('click', () => {
+        $('.my_list').remove();
+    });
+});
