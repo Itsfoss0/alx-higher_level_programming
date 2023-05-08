d53cb4f05b70a91f11f8060ace1517329e2c2203
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-08 18:59:22 +0300

    [Chore]: Add advanced task - 3

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    const langCode = $('input#language_code').val();
+    const API_URL = `https://hellosalut.stefanbohacek.dev/?lang=${langCode}`;
+    $('input#btn_translate').on('click kepress', (event) => {
+      // console.log($('input#language_code').val());
+      // console.log(langCode);
+      // console.log(API_URL);
+      if (event.type === 'click' || (event.type === 'keypress' && event.keyCode === 13)){
+      $.get(API_URL, (reponse, statusCode) => {
+        $('div#hello').text(reponse.hello);
+      });
+    }
+    });
+  });
+  
